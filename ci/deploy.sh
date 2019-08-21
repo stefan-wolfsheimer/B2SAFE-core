e511e9e2fadab4b47539117af528cd0487de8282
Stefan Wolfsheimer <stefan.wolfsheimer@surfsara.nl>
2019-08-21 22:44:39 +0200

    test

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -41,6 +41,9 @@ RPM_PACKAGE=`rpm_package $BUILD `
 IRODS_VERSION=`irods_version $VERSION`
 REPO_NAME=`repo_name $VERSION $GIT_URL $GIT_BRANCH `
 
+set -e
+set -x
+
 ssh $SSH_OPTIONS $YUM_SERVER "mkdir -p /repos/CentOS/7/${REPO_NAME}/Packages/"
 scp $SSH_OPTIONS ./ci/RPMS/Centos/7/${REPO_NAME}/${RPM_PACKAGE} $YUM_SERVER:/repos/CentOS/7/${REPO_NAME}/Packages/
 ssh $SSH_OPTIONS $YUM_SERVER createrepo --update /repos/CentOS/7/${REPO_NAME}
